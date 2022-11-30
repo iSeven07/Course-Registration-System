8a08776871727bec928ebb7cd07da6b92f1a9f13
Aaron Corns <admin@sev7n.dev>
2022-11-29 20:04:02 -0500

    Additional Updates -AC

--- frontend/src/views/AddCourse.vue
+++ frontend/src/views/AddCourse.vue
@@ -1,7 +1,31 @@
 <template>
+    <div class="d-flex justify-content-center p-5 text-center">
 
+        <div class="pt-5">
+            <div class="h3">
+                Add Course
+            </div>
+            <form>
+                <div class="form-row mt-5">
+                    <div class="col">
+                        <label for="courseName">Course Name</label>
+                        <input type="text" class="form-control" id="courseName">
+                    </div>
+                    <div class="col">
+                        <label for="courseTitle">Course Title</label>
+                        <input type="text" class="form-control" id="courseTitle">
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-5">Submit</button>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-
+export default {
+    props: [
+        
+    ]
+}
 </script>
